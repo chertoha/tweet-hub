a559a3505e0c4d3c5deb4697a8d94f72c5596690
Anton <a.chertok@akatech.com.ua>
2023-04-14 19:07:39 +0300

    fuck

--- src/components/Tweets/Tweets.js
+++ src/components/Tweets/Tweets.js
@@ -1,12 +1,13 @@
 import CardList from "components/CardList";
 import Container from "components/Container";
 import PageTitle from "components/PageTitle";
-import { useState } from "react";
+import { Button } from "components/UIKit/Button/Button.styled";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useGetUsersQuery } from "redux/users/usersApi";
 import { ROUTES } from "router";
 
-const REQUEST_LIMIT = 12;
+const DEFAULT_LIMIT = 3;
 const DEFAULT_PAGE = 1;
 
 const Tweets = () => {
@@ -14,24 +15,49 @@ const Tweets = () => {
   const backRoute = location.state?.from ?? ROUTES.HOME;
 
   const [page, setPage] = useState(DEFAULT_PAGE);
+  const [users, setUsers] = useState([]);
 
-  const { data } = useGetUsersQuery({ page, limit: REQUEST_LIMIT });
+  const { data } = useGetUsersQuery({ page: page, limit: DEFAULT_LIMIT });
+
+  const updateArray = (prevArr, newArr) => {
+    prevArr.map((item) => {});
+  };
+
+  useEffect(() => {
+    if (data) {
+      setUsers((prevUsers) => [...prevUsers, ...data.results]);
+    }
+  }, [data]);
 
   if (!data) {
     return;
   }
 
-  console.log(data);
+  console.log(users);
+  // console.log(page);
 
   return (
     <Container>
       <Link to={backRoute}> Back</Link>
       <PageTitle>Users List</PageTitle>
+
+      <CardList list={users} />
+      {/* <CardList list={data.results} /> */}
+
+      <br />
+      <br />
+
+      <Button
+        onClick={() => {
+          setPage((prevPage) => prevPage + 1);
+        }}
+      >
+        Load more
+      </Button>
       <br />
       <br />
       <br />
       <br />
-      <CardList list={data.results} />
     </Container>
   );
 };
