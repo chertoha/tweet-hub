c1867002e4b49d40697fd13028323b2b968da906
Anton <a.chertok@akatech.com.ua>
2023-04-18 09:56:07 +0300

    follow

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,29 +8,53 @@ import {
   ImageWrapper,
   StyledCard,
 } from "./Card.styled";
-import { useUpdateUserMutation } from "redux/users/usersApi";
+import {
+  useGetFavoritesQuery,
+  useLazyGetFavoritesQuery,
+  useUpdateFavoritesMutation,
+  useUpdateUserMutation,
+} from "redux/users/usersApi";
 import { convertNumberToLocale } from "utils/convertNumberToLocale";
+import { useEffect, useState } from "react";
 
-const Card = ({ userData }) => {
-  const [updateUser] = useUpdateUserMutation();
+const Card = ({
+  id,
+  user,
+  tweets,
+  followers,
+  // isFollowing,
+  avatar = defaultAvatar,
+  favorites,
+}) => {
+  const [updateFavorites] = useUpdateFavoritesMutation();
+  const [follow, setFollow] = useState(null);
 
-  const {
-    user,
-    tweets,
-    followers,
-    isFollowing,
-    avatar = defaultAvatar,
-  } = userData;
+  useEffect(() => {
+    const result = favorites.find(({ userId }) => userId === id);
+    setFollow(result);
+  }, [favorites, id]);
+  // useEffect(() => {
+  //   fetchFavorites().then(({ data }) => {
+  //     // const result = favorites.includes(id);
+  //     // console.log(favorites);
+  //     // const result = data.find(({ userId }) => userId === id);
+  //     // setIsFollowing(result);
+  //     // setFollow(result);
+  //   });
+  // }, [favorites, fetchFavorites, id]);
+
+  // useEffect(() => {
+  //   if (favorites) {
+  //     console.log(favorites);
+  //     const result = favorites.find(({ userId }) => userId === id);
+  //     setFollow(result);
+  //   }
+  // }, [favorites, id]);
 
   const onFollowClick = () => {
-    updateUser({
-      ...userData,
-      isFollowing: !isFollowing,
-
-      followers: isFollowing
-        ? (+followers - 1).toString()
-        : (+followers + 1).toString(),
-    });
+    const method = follow ? "DELETE" : "POST";
+    const body = method === "POST" ? { userId: id } : null;
+    updateFavorites({ id: follow?.id, method, body });
   };
 
   return (
@@ -50,12 +74,8 @@ const Card = ({ userData }) => {
           followers === 1 ? "Followers" : "Followers"
         }`}</CardText>
 
-        <CardButton
-          type="button"
-          isActive={isFollowing}
-          onClick={onFollowClick}
-        >
-          {isFollowing ? "Following" : "Follow"}
+        <CardButton type="button" isActive={follow} onClick={onFollowClick}>
+          {follow ? "Following" : "Follow"}
         </CardButton>
       </CardMeta>
     </StyledCard>
@@ -65,12 +85,11 @@ const Card = ({ userData }) => {
 export default Card;
 
 Card.propTypes = {
-  userData: PropTypes.shape({
-    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    user: PropTypes.string.isRequired,
-    tweets: PropTypes.string.isRequired,
-    followers: PropTypes.string.isRequired,
-    isFollowing: PropTypes.bool.isRequired,
-    avatar: PropTypes.string,
-  }).isRequired,
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  user: PropTypes.string.isRequired,
+  tweets: PropTypes.string.isRequired,
+  followers: PropTypes.string.isRequired,
+  // isFollowing: PropTypes.bool.isRequired,
+  avatar: PropTypes.string,
+  favorites: PropTypes.array.isRequired,
 };
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 import Card from "components/Card";
 import { Item, List } from "./CardList.styled";
 
-const CardList = ({ list }) => {
+const CardList = ({ list, favorites }) => {
   return (
     <List>
       {list.map((item) => (
         <Item key={item.id}>
-          <Card userData={item} />
+          <Card {...item} favorites={favorites} />
         </Item>
       ))}
     </List>
